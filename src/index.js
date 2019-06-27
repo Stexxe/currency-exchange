eecd4ad5870b65ddcd8a2ee7f54fd94d33a946e7
Aleksei Tirman <aleksei.tirman@gmail.com>
2019-06-27 19:44:40 +0300

    Функции для пересчета валют

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+const calcRate = (valueFrom, valueTo) => {
+  return valueFrom / valueTo;
+};
+
+const convert = (rate, moneyFrom) => {
+  return rate * moneyFrom;
+};
+
+const exchange = (currencies, currencyFrom, currencyTo, moneyFrom) => {
+  const findCurrency = currency => currencies.find(data => data.code === currency);
+  return convert(
+    calcRate(findCurrency(currencyFrom).value, findCurrency(currencyTo).value),
+    moneyFrom
+  )
+};
+
+const fetchCurrencies = (url) => {
+  return fetch(url).then(response => {
+    return response.json();
+  }).then(json => {
+    return Object.keys(json.Valute).map(code => ({
+      code,
+      name: json.Valute[code].Name,
+      value: json.Valute[code].Value,
+    }))
+  })
+};
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('sw.js', {scope: '/'}).then((e) => {
@@ -9,5 +37,7 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('load', () => {
-  document.body.innerText = 'Offline';
+  fetchCurrencies('https://www.cbr-xml-daily.ru/daily_json.js').then(currencies => {
+    console.log(exchange(currencies, 'USD', 'EUR', 5));
+  });
 });
